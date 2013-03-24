a686f6ec8fa795475332a2d0f1b4f6a0598a2905
kuglisb <mrkugli@gmail.com>
2013-03-24 12:38:51 +0100

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,2 @@
+javac EntryPoint.java Ant.java AntEater.java AntHill.java AntLion.java AntSmell.java Direction.java DynamicElement.java Field.java Food.java FoodSmell.java Gravel.java Killer.java Land.java Map.java Smell.java StaticElement.java Tentacle.java TimeDependent.java Venom.java Water.java
+java EntryPoint
